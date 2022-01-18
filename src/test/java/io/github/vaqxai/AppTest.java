170405a6cc40c829cd0fa60484ca7f27a5690216
umbranium <vakkendwarf@gmail.com>
2022-01-18 15:20:14 +0100

    first commit

---
+++ src/test/java/io/github/vaqxai/AppTest.java
@@ -0,0 +1,18 @@
+package io.github.vaqxai;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author s22666, Stanisław Knapiński
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
