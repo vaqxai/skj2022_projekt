4866e5db5fdb817eb3705bf92813cf0dcb992800
umbranium <vakkendwarf@gmail.com>
2022-01-22 21:46:51 +0100

    cleanup

--- src/main/java/io/github/vaqxai/NetworkResource.java
+++ src/main/java/io/github/vaqxai/NetworkResource.java
@@ -6,7 +6,6 @@ public class NetworkResource {
 
 	private HashMap<String, Integer> owners = new HashMap<>(); // X is owner of 5 counts of this resource, etc...
 	private String identifier = "?"; // Resource's name/identifier
-	private String holder = null; // Address of the resource's holding node TODO: Is this necessary?
 	private int lockedAmount = 0; // How much of the resource is currently locked by other nodes or for processing
 	private int availableAmount = 0; // How much of this resource is currently available for locking
 
@@ -17,7 +16,6 @@ public class NetworkResource {
 
 	public NetworkResource(String identifier, String holder, int amount){
 		this.identifier = identifier;
-		this.holder = holder;
 		this.availableAmount = amount;
 	}
 
--- src/main/java/io/github/vaqxai/ResourceRequest.java
+++ src/main/java/io/github/vaqxai/ResourceRequest.java
@@ -1,6 +1,5 @@
 package io.github.vaqxai;
 
-import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
